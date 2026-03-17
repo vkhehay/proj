d3663c0d770762ff9eae9d164029885793b689ff
vkhehay <v.khehay@gmail.com>
2026-03-17 20:43:42 +0200

    add test database

--- app/config.py
+++ app/config.py
@@ -1,25 +1,48 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 
 class Settings(BaseSettings):
-    db_port: int
-    db_name: str
-    db_password: str
-    db_username: str
-    secret_key: str
-    algorithm: str
-    access_token_exp_min: int
-    db_host: str  # = "db"
+    db_port: int = Field(..., env="DB_PORT")
+    db_name: str = Field(..., env="DB_NAME")
+    db_password: str = Field(..., env="DB_PASSWORD")
+    db_username: str = Field(..., env="DB_USERNAME")
+    secret_key: str = Field(..., env="SECRET_KEY")
+    algorithm: str = Field(..., env="ALGORITHM")
+    access_token_exp_min: int = Field(..., env="ACCESS_TOKEN_EXP_MIN")
+    db_host: str = Field(..., env="DB_HOST")
 
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
+    postgres_user: str = Field(..., env="POSTGRES_USER")
+    postgres_password: str = Field(..., env="POSTGRES_PASSWORD")
+    postgres_db: str = Field(..., env="POSTGRES_DB")
 
-    # env_file: str = ".env" if os.getenv("ENV") != "test" else ".env.test"
-    env_file: str = '.env'
+    model_config = SettingsConfigDict(env_file=".env")
 
-    model_config = SettingsConfigDict(env_file=env_file)
 
+settings = Settings()
+
+# class Settings(BaseSettings):
+#     db_port: int
+#     db_name: str
+#     db_password: str
+#     db_username: str
+#     secret_key: str
+#     algorithm: str
+#     access_token_exp_min: int
+#     db_host: str  # = "db"
+#
+#     postgres_user: str
+#     postgres_password: str
+#     postgres_db: str
+#
+#     # env_file: str = ".env" if os.getenv("ENV") != "test" else ".env.test"
+#     env_file: str = '.env'
+#
+#     class Config:
+#         env_prefix = ""
+#
+#     model_config = SettingsConfigDict(env_file=env_file)
+#
 
 settings = Settings()
